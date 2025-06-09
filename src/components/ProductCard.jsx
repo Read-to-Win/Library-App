7aad5931cd8e0bd3a0a94deffac0881588e5c42d
PreciousXena <precioushenryc@gmail.com>
2025-06-09 10:27:51 +0000

    first commit

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,17 @@
+const ProductCard = () => {
+  return (
+    <div className="bg-blue-400 w-70">
+     <div>
+         <img src="https://picsum.photos/id/1005/200/300" className="w-70" />
+      <p>Book Title: This an image of a man</p>
+      <p>Book Author: Precious Henry</p>
+      <p>Price: $5.00</p>
+        </div>
+
+        
+      
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import { dummyData } from "../../db";
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+ {dummyData.map((book, index) => (
+        <div key={index} className="bg-white rounded-2xl shadow p-4 text-center">
+          <img
+            src={book.imageUrl}
+            alt={book.title}
+            className="w-full h-auto rounded-lg mb-2"
+          />
+          <h3 className="text-lg font-semibold">{book.title}</h3>
+          <p className="text-sm text-gray-600">{book.author}</p>
+        </div>
+      ))}
+    </div>
+
+  )
+}
+
+export default Home
