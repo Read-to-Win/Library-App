4a5d6d256c1fb363a6f3d83f2af43adeff63a8df
Patience Grace Affel <gpatience14@gmail.com>
2025-06-11 11:12:57 +0000

    button changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router";
+
 const Hero = () => {
   return (
     <section id="hero">
       <div className="overlay">
         <h1>A Sword of Storms: Steels and Stones</h1>
-        <button>Browse Books</button>
+        <button><Link to="/books">Browse Books</Link></button>
       </div>
       <div className="background-container">{/* content goes here */}</div>
     
